9e603685656b99b40a0e2205a20acf1c23ca01b9
JerryPC <jeremaios@gmail.com>
2021-02-02 11:11:10 -0300

    Correction _id

--- src/components/Grade.js
+++ src/components/Grade.js
@@ -15,8 +15,8 @@ const Grade = (props) => {
   const getGrade = (id) => {
     GradeDataService.get(id)
       .then((response) => {
-        setCurrentGrade(response.data);
-        console.log(response.data);
+        setCurrentGrade(response.data[0]);
+        console.log(response.data[0]);
       })
       .catch((e) => {
         console.log(e);
@@ -33,7 +33,7 @@ const Grade = (props) => {
   };
 
   const updateGrade = () => {
-    GradeDataService.update(currentGrade.id, currentGrade)
+    GradeDataService.update(currentGrade._id, currentGrade)
       .then((response) => {
         setMessage('The grade was updated successfully!');
       })
@@ -43,7 +43,7 @@ const Grade = (props) => {
   };
 
   const deleteGrade = () => {
-    GradeDataService.remove(currentGrade.id)
+    GradeDataService.remove(currentGrade._id)
       .then((response) => {
         props.history.push('/grade');
       })
